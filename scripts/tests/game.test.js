8f661b4797e4d12451b49087688e371c72f71a70
Kelly Hutchison <kellyhutchison@hotmail.com>
2022-11-23 15:19:06 +0000

    Add choices array

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -26,4 +26,7 @@
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
+    test("choices contains correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
 });
