eacde1bb1641b68d0ddead68780abde814d71352
Kelly Hutchison <kellyhutchison@hotmail.com>
2022-11-24 16:35:07 +0000

    Add event liisteners to buttons

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -56,6 +56,13 @@
      test("should set turnNumber to zero", () => {
         expect(game.turnNumber).toEqual(0);
     });
+    test("expect data-listener to be true", () => {
+        newGame();
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listener")).toEqual("true");
+        }
+    });
  });
 
  describe("gameplay works correctly", () => {
